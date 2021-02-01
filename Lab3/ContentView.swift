c73096920d067d03f4cccdb968caf4c4cfaf5766
ics20-073 <ics20-073@Camosun.bc.ca>
2021-02-01 12:35:06 -0800

    Start of Lab 3, duplicate of Lab 1

--- Lab3/ContentView.swift
+++ Lab3/ContentView.swift
@@ -6,13 +6,101 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
+//Toggleview class, press to enable button press
+struct ToggleView: View {
+    var pushEnabled: Binding<Bool>
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Toggle("ToggleSwitch", isOn: pushEnabled).labelsHidden()
     }
 }
+//Button class, press to increase text field by one. Will only
+struct ButtonView: View {
+    var pushCount: Binding<Int>
+    var pushEnabled: Bool
+    var body: some View {
+        Button(action: {
+            self.pushCount.wrappedValue &+= 1
+            if(self.pushCount.wrappedValue < 0)
+            {
+                self.pushCount.wrappedValue = 0
+            }
+            UIApplication.shared.windows.forEach {                 $0.endEditing(true)
+            }
+        }) {
+            Text("Push")
+        }
+        .disabled(!pushEnabled)
+    }
+}
+//TextField Class, type in here using the app keyboard.
+//The input will ignore 0's before value, and letters and will give back a
+// number value in the text
+struct TextFieldView: View {
+    var offset: Binding<String>
+    var pushCount: Binding<Int>
+    var body: some View {
+        TextField("Number of Pushes",text:Binding(
+            get: {
+                self.offset.wrappedValue
+            },
+            set: {
+                newValue,_ in
+                self.offset.wrappedValue=""
+                self.pushCount.wrappedValue = Int(newValue) ?? 0
+                print(self.pushCount.wrappedValue)
+                self.offset.wrappedValue = String(self.pushCount.wrappedValue)
+                    
+                
+            }
+        ))
+        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+    
+}
+
+//The text class will display the number value converted in the text field
+struct TextView: View {
+    var pushCount: Int
+    var body: some View {
+        Text(String(pushCount))
+            .padding(-2.0)
+    }
+}
+//All of the prior classes are passed through Content view class.
+// With Hstack, the app will have different settings with both an Iphone
+// as well as an Ipad Screen Size
+struct ContentView: View {
+    @State var pushCount: Int = 0
+    @State var pushEnabled: Bool = false
+    @State var offset: String = ""
+    @Environment(\.horizontalSizeClass) var sizeClass
+    var body: some View {
+        VStack() {
+            if sizeClass == .regular {
+                Spacer()
+                ToggleView(pushEnabled: $pushEnabled)
+                Spacer()
+                ButtonView(pushCount: $pushCount, pushEnabled: pushEnabled)
+                Spacer()
+                TextFieldView(offset: $offset, pushCount: $pushCount)
+                Spacer()
+                TextView(pushCount: pushCount)
+                Spacer()
+            }
+            else {
+                HStack() {
+                    ToggleView(pushEnabled: $pushEnabled)
+                    ButtonView(pushCount: $pushCount, pushEnabled: pushEnabled)
+                }
+                TextFieldView(offset: $offset, pushCount: $pushCount)
+                TextView(pushCount: pushCount)
+            }
+            
+            
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Lab3UITests/Lab3UITests.swift
+++ Lab3UITests/Lab3UITests.swift
@@ -1,8 +1,8 @@
 //
-//  Lab3UITests.swift
-//  Lab3UITests
+//  Lab1UITests.swift
+//  Lab1UITests
 //
-//  Created by ICS 224 on 2021-02-01.
+//  Created by ICS 224 on 2021-01-11.
 //
 
 import XCTest
@@ -10,6 +10,8 @@ import XCTest
 class Lab3UITests: XCTestCase {
 
     override func setUpWithError() throws {
+        
+        XCUIApplication().launch()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -22,21 +24,120 @@ class Lab3UITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    
+    
     func testExample() throws {
         // UI tests must launch the application that they test.
+        // To test the Simulator with a basic "1" and "0" then pressing the button one
         let app = XCUIApplication()
-        app.launch()
-
+        
+        
+        app.switches["ToggleSwitch"].tap()
+        app.textFields["Number of Pushes"].tap()
+        
+        let moreKey = app.keyboards.children(matching: .other).element.children(matching: .other).element/*@START_MENU_TOKEN@*/.children(matching: .key).matching(identifier: "more").element(boundBy: 0)/*[[".children(matching: .key).matching(identifier: \"numbers\").element(boundBy: 0)",".children(matching: .key).matching(identifier: \"more\").element(boundBy: 0)"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        moreKey.tap()
+       
+        
+        let key = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key.tap()
+     
+        
+        let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key2.tap()
+   
+        app.buttons["Push"].tap()
+        
+       
+      
+        XCTAssertEqual(app.staticTexts.firstMatch.label, "11")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testletters() throws {
+        // UI tests must launch the application that they test.
+        // Testing letters by entering letters on keyboard, switching to number keyboard then entering numbers to show that letters will not be inputted in text field
+        let app = XCUIApplication()
+        
+        
+        app.switches["ToggleSwitch"].tap()
+        app.textFields["Number of Pushes"].tap()
+        
+        let moreKey = app.keyboards.children(matching: .other).element.children(matching: .other).element/*@START_MENU_TOKEN@*/.children(matching: .key).matching(identifier: "more").element(boundBy: 0)/*[[".children(matching: .key).matching(identifier: \"numbers\").element(boundBy: 0)",".children(matching: .key).matching(identifier: \"more\").element(boundBy: 0)"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+       
+        
+        app.keys["A"].tap()
+        app.keys["b"].tap()
+        moreKey.tap()
+        app.keys["4"].tap()
+        app.keys["2"].tap()
+        
+     
+       
+   
+        
+        
+       
+      
+        XCTAssertEqual(app.staticTexts.firstMatch.label, "42")
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testZeros() throws {
+        // UI tests must launch the application that they test.
+        //testing to make sure 0s do not get inputted before 1-9 numbers
+        let app = XCUIApplication()
+        
+        
+        app.switches["ToggleSwitch"].tap()
+        app.textFields["Number of Pushes"].tap()
+        
+        let moreKey = app.keyboards.children(matching: .other).element.children(matching: .other).element/*@START_MENU_TOKEN@*/.children(matching: .key).matching(identifier: "more").element(boundBy: 0)/*[[".children(matching: .key).matching(identifier: \"numbers\").element(boundBy: 0)",".children(matching: .key).matching(identifier: \"more\").element(boundBy: 0)"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+       
+        moreKey.tap()
+        app.keys["0"].tap()
+        app.keys["0"].tap()
+        
+        app.keys["7"].tap()
+        app.keys["7"].tap()
+        XCTAssertEqual(app.staticTexts.firstMatch.label, "77")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testOverflow() throws {
+        // UI tests must launch the application that they test.
+        // Testing Overflow by entering large number with keyboard then pushing to make sure the text field goes back to zero
+        
+        let app = XCUIApplication()
+        
+        
+        app.switches["ToggleSwitch"].tap()
+        app.textFields["Number of Pushes"].tap()
+        
+        let moreKey = app.keyboards.children(matching: .other).element.children(matching: .other).element/*@START_MENU_TOKEN@*/.children(matching: .key).matching(identifier: "more").element(boundBy: 0)/*[[".children(matching: .key).matching(identifier: \"numbers\").element(boundBy: 0)",".children(matching: .key).matching(identifier: \"more\").element(boundBy: 0)"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+       //9223372036854775807
+        moreKey.tap()
+        app.typeText("9223372036854775807")
+        
+        app.buttons["Push"].tap()
+        
+        XCTAssertEqual(app.staticTexts.firstMatch.label, "0")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
+
